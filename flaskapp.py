973c1ba9978acf32d016f323afee997cef9498ed
spankratov <ss-pankratov@yandex.ru>
2016-07-24 21:34:46 +0300

    getting messages template

--- flaskapp.py
+++ flaskapp.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 
 import atexit
 import telegram_api
+import logging
 
 app = Flask(__name__)
 app.config.from_pyfile('default_config.py')
@@ -14,6 +15,13 @@ if app.debug is not True:
 telegram_api.set_webhook(app.config['TELEGRAM_URL'], app.config['WEBHOOK_URL'], app.config['CERTIFICATE'])
 atexit.register(telegram_api.reset_webhook, telegram_url=app.config['TELEGRAM_URL'],
                 certificate=app.config['CERTIFICATE'])
+
+
+@app.route('/webhook/' + app.config['BOT_TOKEN'], methods=['POST'])
+def webhook_handler():
+    if request.method == "POST":
+        logging.info("Got the message.\nHeaders:\n%s\nData:\n%s\n" % (request.headers, request.data))
+    return "Got the message"
 
 if __name__ == '__main__':
     app.run()
