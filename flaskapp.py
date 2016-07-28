e5cf5af04e856d98b080f2d5e70eee646c044fea
spankratov <ss-pankratov@yandex.ru>
2016-07-28 19:13:29 +0300

    fixed view

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,7 @@
 from application import handlers, Application
 
 app = Application(handlers)
+flask_app = app.flask_app
 celery = app.celery()
 
 import tasks
--- views/reminders.py
+++ views/reminders.py
@@ -1,10 +1,11 @@
 import logging
-from flask import current_app, request
+from flask import request
 from flask.views import MethodView
+from flaskapp import app
 
 
 class UpdatePost(MethodView):
 
     def post(self):
         logging.info("Got the update:\n%s" % request.data)
-        return current_app.bot.handle_update(request.get_json())
+        return app.bot.handle_update(request.get_json())
