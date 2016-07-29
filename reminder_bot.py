c2fd69c6a61a1d858f29ed01e465b7167d7139bc
spankratov <ss-pankratov@yandex.ru>
2016-07-30 00:48:59 +0300

    added datetime detecting, LICENSE file and tests

--- reminder_bot.py
+++ reminder_bot.py
@@ -1,8 +1,11 @@
 import logging
 import atexit
+import re
+from dateutil.parser import parse as parse_datetime
+from dateutil.relativedelta import relativedelta
 from telegram_api import TelegramApi
 from pymongo import MongoClient
-from datetime import datetime, timedelta
+from datetime import datetime
 
 
 class PersonalReminderBot:
@@ -13,6 +16,7 @@ class PersonalReminderBot:
         self.reminder_text = config['DEFAULT_REMINDER_TEXT']
         self.message_types = config['DEFAULT_MESSAGE_TYPES']
         self.delay = config['DEFAULT_REMINDER_DELAY']
+        self.construct_re_patterns()
 
     def set_webhook(self):
         logging.info("Starting the app")
@@ -25,11 +29,113 @@ class PersonalReminderBot:
 
         atexit.register(stop_bot)
 
+    def construct_re_patterns(self):
+        self.dates_words = ['year', 'month', 'week', 'day', 'hour', 'minute', 'second']
+        in_general = r'(((?P<{0}>1) {0}|(?P<{0}s>\d+) {0}s))'
+        in_patterns =[]
+        for word in self.dates_words:
+            in_patterns.append(in_general.format(word))
+
+        self.in_altogether_regex = re.compile(r'in ' + r'(' + r'|'.join([pattern + r',? ?' for pattern in in_patterns]) + r')+', re.IGNORECASE)
+        self.words_pattern = re.compile(r'(?P<word_date>tomorrow|tonight|today|day after tomorrow|next week|next month|next year|end of this week)', re.IGNORECASE)
+
+    def detect_datetime(self, text, user_now):
+        result = user_now
+        changed = False
+
+        matches = re.finditer(self.in_altogether_regex, text)
+        for match in matches:
+            changed = True
+            for word in self.dates_words:
+                word_value = match.group(word) or match.group(word + 's')
+                if word_value:
+                    result += relativedelta(**{word + 's': int(word_value)})
+        if changed:
+            return result
+
+        match = re.search(self.words_pattern, text)
+        if match:
+            word = match.group()
+            result, flags = self.process_word_for_date(word, result)
+            changed = True
+        else:
+            flags = {'year': False, 'month': False, 'day': False, 'time': False}
+
+        try:
+            parsed_datetime, tokens = parse_datetime(text, default=result, dayfirst=True, fuzzy_with_tokens=True)
+        except ValueError:
+            if changed:
+                return result
+            else:
+                return user_now + relativedelta(microseconds=self.delay)
+
+        # test if parsed string contains only digits
+        # dateutil.parser recognise alone digits as the part of the date
+        # e.g. "catch 6 monkeys" with default=datetime(2016, 7, 29, 18, 20, 44, 667349) is parsed as datetime(2016, 7, 6, 18, 20, 44, 667349)
+        parsed_string = text
+        for token in tokens:
+            parsed_string = parsed_string.replace(token, '')
+        match = re.match(r'^[\d\d]+$', parsed_string)
+        if match:
+            return result
+
+        if not flags['year']:
+            result = result.replace(year=parsed_datetime.year)
+        if not flags['month']:
+            result = result.replace(month=parsed_datetime.month)
+        if not flags['day']:
+            result = result.replace(day=parsed_datetime.day)
+        if not flags['time']:
+            result = result.replace(hour=parsed_datetime.hour, minute=parsed_datetime.minute, second=parsed_datetime.second, microsecond=parsed_datetime.microsecond)
+        return result
+
+    @staticmethod
+    def process_word_for_date(word, now):
+        if word == 'tomorrow':
+            tomorrow = now + relativedelta(days=1)
+            return tomorrow, {'year': True, 'month': True, 'day': True, 'time': False}
+        elif word == 'tonight':
+            tonight = datetime(year=now.year, month=now.month, day=now.day, hour=22)
+            if now > tonight:
+                return now, {'year': True, 'month': True, 'day': True, 'time': True}
+            else:
+                return tonight, {'year': True, 'month': True, 'day': True, 'time': True}
+        elif word == 'today':
+            today = now + relativedelta(hours=1)
+            if now.day == today.day:
+                return today, {'year': True, 'month': True, 'day': True, 'time': True}
+            else:
+                return datetime(year=now.year, month=now.month, day=now.day, hour=23, minute=59, second=59), {'year': True, 'month': True, 'day': True, 'time': True}
+        elif word == 'day after tomorrow':
+            tomorrow = now + relativedelta(days=2)
+            return tomorrow, {'year': True, 'month': True, 'day': True, 'time': False}
+        elif word == 'next week':
+            next_week = now + relativedelta(weeks=1)
+            return next_week, {'year': False, 'month': False, 'day': False, 'time': False}
+        elif word == 'next month':
+            next_month = now + relativedelta(months=1)
+            return next_month, {'year': True, 'month': True, 'day': False, 'time': False}
+        elif word == 'next year':
+            next_year = now + relativedelta(years=1)
+            return next_year, {'year': True, 'month': False, 'day': False, 'time': False}
+        elif word == 'end of this week':
+            end_of_week = now - relativedelta(days=now.weekday()) + relativedelta(days=6)
+            return end_of_week, {'year': True, 'month': True, 'day': True, 'time': False}
+
+
     def handle_update(self, update):
         if 'message' in update:
             message = update['message']
             chat_id = message['chat']['id']
-            note = {'chat_id': chat_id, 'due': datetime.utcnow() + timedelta(microseconds=self.delay)}
+            note = {'chat_id': chat_id}
+            if 'text' in message:
+                predicted_due = self.detect_datetime(message['text'], datetime.utcnow())
+                if predicted_due < datetime.utcnow():
+                    note['due'] = datetime.utcnow() + relativedelta(microseconds=self.delay)
+                else:
+                    note['due'] = predicted_due
+            else:
+                note['due'] = datetime.utcnow() + relativedelta(microseconds=self.delay)
             for send_type in self.message_types:
                 if send_type in message:
                     note['type'] = send_type
@@ -46,5 +152,6 @@ class PersonalReminderBot:
                     mongo_client.close()
                     from tasks import remind
                     remind.apply_async(args=[str(inserted_note.inserted_id)], eta=note['due'])
+                    self.telegram_api.send_message(note['chat_id'], 'Ok! You will be reminded at ' + note['due'].strftime("%Y-%m-%d %H:%M:%S"))
                     break
         return "Got the message"
---
+++ tests/detect_datetime_test.py
@@ -0,0 +1,45 @@
+import unittest
+from reminder_bot import PersonalReminderBot
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
+class TestDetectDatetime(unittest.TestCase):
+    def setUp(self):
+        config = {'TELEGRAM_URL': '', 'CERTIFICATE': '', 'MONGO_DEFAULT_BD': '', 'WEBHOOK_URL': '',
+                  'DEFAULT_REMINDER_TEXT': '', 'DEFAULT_MESSAGE_TYPES': '',
+                  'DEFAULT_REMINDER_DELAY': 1000 * 1000 * 3600 * 24}
+        self.bot = PersonalReminderBot(config)
+
+    def test_ins(self):
+        dt = datetime.now()
+        detected = self.bot.detect_datetime('in 3 years remind me in 10 minutes', dt)
+        self.assertEqual(detected, dt + relativedelta(years=3, minutes=10))
+        detected = self.bot.detect_datetime('remind me in 3 years, 1 month, 56 weeks, 3 seconds', dt)
+        self.assertEqual(detected, dt + relativedelta(years=3, months=1, weeks=56, seconds=3))
+        detected = self.bot.detect_datetime('in this chat remind me in 3 years, 1 month, 56 weeks, 3 seconds', dt)
+        self.assertEqual(detected, dt + relativedelta(years=3, months=1, weeks=56, seconds=3))
+        detected = self.bot.detect_datetime('in 1 year, 3 seconds in 10 seconds remind me in 5 minutes', dt)
+        self.assertEqual(detected, dt + relativedelta(years=1, minutes=5, seconds=13))
+
+    def test_words(self):
+        dt = datetime.now()
+        detected = self.bot.detect_datetime('remind me tomorrow', dt)
+        self.assertEqual(detected, dt + relativedelta(days=1))
+        detected = self.bot.detect_datetime('remind me tomorrow at 8 pm', dt)
+        self.assertEqual(detected, (dt + relativedelta(days=1)).replace(hour=20))
+        dt = datetime(year=2016, month=7, day=30, hour=23, minute=30)
+        detected = self.bot.detect_datetime('today reminder would be nice', dt)
+        self.assertEqual(detected, dt.replace(hour=23, minute=59, second=59, microsecond=0))
+        detected = self.bot.detect_datetime('remind me next week at sunday', dt)
+        self.assertEqual(detected, dt.replace(month=8, day=7))
+        detected = self.bot.detect_datetime('remind me next year at 7 Jan 17:33', dt)
+        self.assertEqual(detected, dt.replace(year=2017, month=1, day=7, hour=17, minute=33))
+
+    def test_parse(self):
+        dt = datetime.now()
+        detected = self.bot.detect_datetime('reminder at Thu Sep 25 10:36:28 2003', dt)
+        self.assertEqual(detected, datetime(2003, 9, 25, 10, 36, 28))
+
+if __name__ == '__main__':
+    unittest.main()
